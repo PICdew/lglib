01cf585c561cd3be08ea6ef57acb810a517f5ffa
Fernando Rodriguez <frodriguez.developer@outlook.com>
2014-09-26 00:40:19 -0400

    Added support for building on linux

---
+++ ili9341/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+make \
+	CC=/opt/microchip/xc16/v1.21/bin/xc16-gcc \
+	AS=/opt/microchip/xc16/v1.21/bin/xc16-as \
+	AR=/opt/microchip/xc16/v1.21/bin/xc16-ar \
+	RM="rm -f" \
+	OPT=-O1 \
+	HAL=../../fat32lib/dspic_hal \
+	$@
